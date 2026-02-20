9e7ea60eb177db48af9395347f2c4f331f1e7226
MeAnjanaS <157295734+MeAnjanaS@users.noreply.github.com>
2026-02-20 18:53:11 +0530

    Implement substring search function in first.c
    Added a function to find a substring in a string and updated main to demonstrate its usage.

--- first.c
+++ first.c
@@ -1,18 +1,27 @@
-/*#include <stdio.h>
-
-int main() {
-    puts("First c Program");
-    return 0;
-}*/
-
 #include <stdio.h>
-
+#include<string.h>
+int findsubstr(char str[],char target[]){
+    int n=strlen(str);
+    int m=strlen(target);
+    for(int i=0;i<=n-m;i++){
+        int j;
+        for( j=0;j<m;j++){
+            if(str[i+j]!=target[j]){
+               break;
+            }
+        }
+        if(j==m) return i;
+    }
+    return -1;
+}
 int main() {
-    char str[] = "First c Program";
-    
-    for(int i = 0; str[i] != '\0'; i++) {
-        putchar(str[i]);
+    char str[]="hello World world";
+    char target[]="world";
+    int idx=findsubstr(str,target);
+    if(idx!=-1){
+        printf("Found at index %d",idx);
+    }else{
+        printf("Not found");
     }
 
-    return 0;
 }
