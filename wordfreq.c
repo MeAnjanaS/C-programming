383b2eb37a78920fb7ed13a56eed4c1969dab268
MeAnjanaS <157295734+MeAnjanaS@users.noreply.github.com>
2026-02-24 09:39:49 +0530

    Update wordfreq.c

--- wordfreq.c
+++ wordfreq.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
-#define MAX_WORDS 100
-#define MAX_LEN 50
-
-void normalize(char *str);
-void countWords(char *text, char words[][MAX_LEN], int freq[], int *uniqueCount);
-int findWord(char words[][MAX_LEN], int uniqueCount, char *word);
-int main() {
-    char paragraph[500];
-    char words[MAX_WORDS][MAX_LEN];
-    int freq[MAX_WORDS] = {0};
-    int uniqueCount = 0;
-    printf("Enter a paragraph:\n");
-    fgets(paragraph, sizeof(paragraph), stdin);
-    normalize(paragraph);
-    countWords(paragraph, words, freq, &uniqueCount);
+#define MAX 50
+#define LEN 20
+struct Word{
+    char word[LEN];
+    int count;
+};
+void countFreq(char input[][LEN],int n,struct Word words[],int *unicount) {
+    int found=0;
+    for(int j=0;i<*unicount;j++){
+        if (strcmp(words[j].word,input[i])==0) {
+            words[j].count++;
+            found=1;
+            break;
+        }
+    }
+    if(!found){
+            strcpy(words[*unicountt].word,input[i]);
+            words[*unicount].count=1;
+            (*unicount)++;
+        }
+    }
+}
+int main(){
+    char input[MAX][LEN};
+    struct Word words[MAX];
+    int n;
+    int unicount=0;
+    printf("Enter no.of words:\n");
+    scanf("%d",&n);
+    printf("Enter words:\n");
+    for(int i=0;i<n;i++) {
+        scanf("%s",input[i]);
+    }
+    countFreq(input,n,words,&unicount);
     printf("\nWord Frequencies:\n");
-    for(int i = 0; i < uniqueCount; i++) {
-        printf("%s : %d\n", words[i], freq[i]);
+    for(int i=0;i<unicount;i++) {
+        printf("%s : %d\n",words[i].word,words[i].count);
     }
     return 0;
 }
+
+
+
